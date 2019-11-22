5258cb72d080e32c2ac685a02cfba7ae21192fc1
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-22 14:08:43 -0800

    progress

--- port/src/Components/Home.js
+++ port/src/Components/Home.js
@@ -6,12 +6,15 @@ class Home extends Component {
     render(){
         return (
             <div className="home">
-              <h1 className="intro">Hello! I'm Lauren Robinson. <br/> I am a passionate software engineer located in Boston, Massachusettes.</h1> 
+                <h1 className ="hello">Hello, there! <img className="link3"src="https://huntpng.com/images250/sherlock-smiley-face-png-1.png"/></h1>
+                <img src="https://i.paste.pics/d123c339e124a4ec80caf37b70964d62.png" className="profile"/>
+              <h1 className="intro">I'm Lauren Robinson. <br/> I am a passionate software engineer located in Boston, Massachusettes. <br/>
+              Get to know me better! Click one of the links below or contact me at laurenhaleyrobinson@gmail.com!</h1> 
               
-              <img src="https://i.paste.pics/d123c339e124a4ec80caf37b70964d62.png" className="profile"/>
-              <a href=""><img className="link" src="https://icon-library.net/images/github-icon-png/github-icon-png-22.jpg"/></a>
-              <a href=""><img className="link2" src="https://retohercules.com/images/linkedin-logo-transparent-background-2.png"/></a>
-              <img className="link3"src="https://huntpng.com/images250/sherlock-smiley-face-png-1.png"/>
+         
+              <a href="https://github.com/l-dev"><img className="link" src="https://icon-library.net/images/github-icon-png/github-icon-png-22.jpg"/></a>
+              <a href="https://www.linkedin.com/in/laurrobinson/"><img className="link2" src="https://retohercules.com/images/linkedin-logo-transparent-background-2.png"/></a>
+             
             </div>
         )
     }
--- port/src/Components/Land.js
+++ port/src/Components/Land.js
@@ -7,6 +7,7 @@ render(){
         <div className="Land">
             <h1 className="welcome">WELCOME</h1>
             <h1 className="welcome2">WELCOME</h1>
+           <a href="/work"><h1 className="arrow">CHECK OUT MY PROJECTS &rarr; &rarr; &rarr; &rarr;</h1></a> 
         </div>
     )
 }
