d876ff00a6cf6a78097a01fa9b73f3dee0ad5288
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-26 12:03:39 -0800

    Updates

---
+++ precache-manifest.4cb015900b174c8724b93cc1a9b232b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463ba370a51f43dd5a4652238fe0e840",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "b06217ec6cc1c421e1a4",
+    "url": "/port/static/css/main.0f46ad7e.chunk.css"
+  },
+  {
+    "revision": "ae0b7d6d9e3dc3d139f9",
+    "url": "/port/static/js/2.1aeee87b.chunk.js"
+  },
+  {
+    "revision": "b06217ec6cc1c421e1a4",
+    "url": "/port/static/js/main.ac4919ae.chunk.js"
+  },
+  {
+    "revision": "2226386b48eb32775ada",
+    "url": "/port/static/js/runtime-main.ba58189e.js"
+  }
+]);
