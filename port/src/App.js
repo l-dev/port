91156b52baa0d297911d1f90625dcb41bce7d3d6
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-26 15:59:40 -0800

    dep

--- port/src/App.js
+++ port/src/App.js
@@ -1,28 +1,26 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Route, Link, Switch } from "react-router-dom";
-import Land from "./Components/Land"
+import { Route, Link, Switch } from "react-router-dom";
+import Land from "./Components/Land";
 import Home from "./Components/Home";
-import Work from "./Components/Work"
-
+import Work from "./Components/Work";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <nav className="nav">
-          <a href="/">HOME</a>
-          <a href="/home">ABOUT</a>
-          <a href="/work">PROJECTS &rarr;</a>
+          <a href={process.env.PUBLIC_URL + "/"}>HOME</a>
+          <a href={process.env.PUBLIC_URL + "/home"}>ABOUT</a>
+          <a href={process.env.PUBLIC_URL + "/work"}>PROJECTS &rarr;</a>
         </nav>
       </header>
-      <HashRouter>
+
       <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/" component={Land}/>
-        <Route exact path="/work" component={Work}/>
+        <Route exact path={process.env.PUBLIC_URL + "/home"} component={Home} />
+        <Route exact path={process.env.PUBLIC_URL + "/"} component={Land} />
+        <Route exact path={process.env.PUBLIC_URL + "/work"} component={Work} />
       </Switch>
-      </HashRouter>
     </div>
   );
 }
