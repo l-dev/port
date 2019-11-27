6ddaae4090541ba272a14b9e898145a97019e739
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-26 16:05:51 -0800

    icon

--- port/src/App.js
+++ port/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Link, Switch } from "react-router-dom";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
 import Land from "./Components/Land";
 import Home from "./Components/Home";
 import Work from "./Components/Work";
@@ -15,12 +15,21 @@ function App() {
           <a href={process.env.PUBLIC_URL + "/work"}>PROJECTS &rarr;</a>
         </nav>
       </header>
-
-      <Switch>
-        <Route exact path={process.env.PUBLIC_URL + "/home"} component={Home} />
-        <Route exact path={process.env.PUBLIC_URL + "/"} component={Land} />
-        <Route exact path={process.env.PUBLIC_URL + "/work"} component={Work} />
-      </Switch>
+      <HashRouter>
+        <Switch>
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/home"}
+            component={Home}
+          />
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Land} />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/work"}
+            component={Work}
+          />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
